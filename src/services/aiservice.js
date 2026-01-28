74da2b6420fb0989990ab65af498c2d779518309
Kaushal Shukla <103252985+Delltronex@users.noreply.github.com>
2026-01-29 01:34:20 +0530

    Update aiservice.js

--- src/services/aiservice.js
+++ src/services/aiservice.js
@@ -1,6 +1,5 @@
 export async function getAIDietPlan(formData, results) {
-  const res = await fetch("https://diet-server-oj8p.onrender.com/api/ai
-", {
+  const res = await fetch("https://diet-server-oj8p.onrender.com/api/ai", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ formData, results }),
