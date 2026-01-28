0a7d45799f194fd2163dc0081161a335eeeedcea
Kaushal Shukla <103252985+Delltronex@users.noreply.github.com>
2026-01-29 03:21:20 +0530

    Integrate VitePWA plugin into Vite config
    Added PWA support with manifest and assets.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "tron.jpg",
+        "tron1.avif",
+        "Kaushal_R.pdf"
+      ],
+      manifest: {
+        name: "NutriLife",
+        short_name: "NutriLife",
+        description: "Personal AI Diet planer website",
+        theme_color: "#00E5FF",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/e.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/a.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
+
+  // Required for Vercel
+  build: {
+    outDir: "dist"
+  }
+});
